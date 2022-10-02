6804dde14268e6fd39653d62da740e91f95ca44c
hitman111 <ganibal1993@gmail.com>
2022-10-02 23:33:32 +0500

    задачи с 0 по 3

---
+++ Zadachi/Zadacha03/Program.cs
@@ -0,0 +1,22 @@
+﻿int NumberEntry()
+{
+    Console.WriteLine("Введите номер дня недели");
+    int d = Convert.ToInt32(Console.ReadLine());
+    return d;
+}
+
+bool Checking(int n)
+{
+    return (n >0 && n <8);
+}
+
+string [] Sunday = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+int d = NumberEntry();
+if (Checking(d))
+{
+    Console.WriteLine($"Заданый день - {Sunday[d-1]}");
+}
+else
+{
+    Console.WriteLine("Заданный день должен лежать в интервале от 1 до 7");
+}
