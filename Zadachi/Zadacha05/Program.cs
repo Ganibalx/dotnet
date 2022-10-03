b2fac2dc8ae44c16e8996e4ed7aa4bcc0e69d8a8
hitman111 <ganibal1993@gmail.com>
2022-10-04 00:08:52 +0500

    Добавил задачи 4, 5

---
+++ Zadachi/Zadacha05/Program.cs
@@ -0,0 +1,14 @@
+﻿double Сorner()
+{
+    Console.WriteLine("Введите a для поиска y = f(a)");
+    double a = Convert.ToDouble(Console.ReadLine());
+    return a;
+}
+
+double Сalculation(double a)
+{
+    double y = Math.Sin(a)+(1-Math.Cos(a));
+    return y;
+}
+
+Console.WriteLine(Math.Round(Сalculation(Сorner()),3));
