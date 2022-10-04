f9b7894bda76f8b8e5bd228f6da6f9f336e20b2f
hitman111 <ganibal1993@gmail.com>
2022-10-05 00:00:05 +0500

    Добавил задачи 8,9

---
+++ Zadachi/Zadacha09/Program.cs
@@ -0,0 +1,12 @@
+﻿int Input()
+{
+    Console.WriteLine("Введите трехзначное число");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Output(int N)
+{
+    Console.WriteLine("Третье число - " + N % 10 );
+}
+
+Output(Input());
