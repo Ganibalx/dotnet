3b858910d410af7565b1c801b01e323347d8d2b3
unknown <@DESKTOP-HIA1QA4.rosseti.ural>
2022-10-04 16:50:17 +0500

    добавил 7 задачу

---
+++ Zadachi/Zadacha07/Program.cs
@@ -0,0 +1,18 @@
+﻿int Input()
+{
+    Console.WriteLine("Введите N");
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+
+void Print(int N)
+{
+ int i = -N;
+ while (i <= N)
+ {
+    Console.Write($"{i}, ");
+    i ++;
+ }
+}
+
+Print(Input());
