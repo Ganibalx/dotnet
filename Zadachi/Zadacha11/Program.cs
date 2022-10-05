96cb61399db71a2244e43e0379d18e4e45ba9b8d
hitman111 <ganibal1993@gmail.com>
2022-10-05 23:30:56 +0500

    Добавил пол задачи 11

---
+++ Zadachi/Zadacha11/Program.cs
@@ -0,0 +1,29 @@
+﻿int RandomNumber()
+{
+
+}
+
+void Output(int n)
+{
+    int a = n/10;
+    int b = n%10;
+    if (a=b)
+    {
+        Console.WriteLine(&"Числа равны {a}");
+    }
+    else
+    {
+        if (a>b)
+        {
+            Console.WriteLine(&"Наибольшее число {a}");
+        }
+        else
+        {
+            Console.WriteLine(&"Наибольшее число {b}");
+        }
+    }
+}
+
+int n = RandomNumber();
+Console.WriteLine($"заданное число {n}");
+Output(n);
