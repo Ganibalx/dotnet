4108c8cfddf50489d4d400a7db159cd0eaf068b3
hitman111 <ganibal1993@gmail.com>
2022-10-07 22:12:55 +0500

    сделал дз к 2 семинару

--- Seminari/2/HomeWork/Zadacha10/Program.cs
+++ Seminari/2/HomeWork/Zadacha10/Program.cs
@@ -1,3 +1,12 @@
-﻿int a = 976;
-int b = (a % 100)/10;
-Console.WriteLine("Вторая цифра равна - " + b);
+﻿int Input()
+{
+    Console.WriteLine("Введите трехзначное число");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Calculation(int n)
+{
+    Console.WriteLine("Вторая цифра равна - " + (n % 100)/10);
+}
+
+Calculation(Input());
