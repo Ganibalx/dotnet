01c4a9c6ca0fe8c0f36f93afba4ca85b80f107d0
Ganibalx <Ganibal1993@gmail.com>
2022-10-10 13:19:00 +0500

    Сделал ДЗ к 4 семинару

---
+++ Seminari/4/HomeWork/Zadacha27/Program.cs
@@ -0,0 +1,25 @@
+﻿int Input()
+{
+    int i = 0;
+    bool falg = false;
+    while (!falg)
+    {
+        Console.WriteLine("Введите число");
+        falg = int.TryParse(Console.ReadLine(), out i);
+    }
+    return (Math.Abs(i));
+}
+
+int Calculation(int a)
+{
+    int result = 0;
+    while(a>0)
+    {
+        result+=a%10;
+        a/=10;
+    }  
+    return result;
+}
+
+int a = Input();
+Console.WriteLine($"{a} -> {Calculation(a)}");
---
+++ Seminari/4/HomeWork/Zadacha29/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] Generation()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+void print(int[] array)
+{
+    Console.WriteLine(string.Join(' ', array));
+}
+
+print(Generation());
