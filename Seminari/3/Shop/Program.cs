1b96cb9be2df2749eedc1b8bce64e7f2f06d2a41
hitman111 <ganibal1993@gmail.com>
2022-10-10 20:57:54 +0500

    ешил задачу про магазин

--- Seminari/3/Shop/Program.cs
+++ Seminari/3/Shop/Program.cs
@@ -1,51 +1,52 @@
-﻿int InputVisitors() 
+﻿int InputVisitors()
 {
-    return new Random().Next(1,100); 
+    return new Random().Next(1, 20);
 }
 
-string InputTime ()
+string InputTime()
 {
-    int a = new Random().Next(0,23);
-    int b = new Random().Next(0,23);
+    int a = new Random().Next(0, 23);
+    int b = new Random().Next(0, 23);
     string result = $"{a}-{b}";
-    if(a==b)
+    if (a == b)
     {
         result = $"{a}";
     }
     else
     {
-        if (a>b)
+        if (a > b)
         {
             result = $"{b}-{a}";
         }
     }
-    return result
+    return result;
 }
 
 int [] Transformation(string a)
 {
-    if(a.Length>2)
+    if(a.Length > 2)
     {
-    string [] s = a.Split('-');
-    int i = Convert.ToInt32(s[1])-Convert.ToInt32(s[0])+1;
-    int [] result = new int [i];
-    for(int j = 0; j<result.Length; j++)
-    {
-        result[i] = Convert.ToInt32(s[0])+j;
-    }
+        string[] s = a.Split('-');
+        int i = Convert.ToInt32(s[1]) - Convert.ToInt32(s[0]) + 1;
+        int[] result = new int[i];
+        for (int j = 0; j < result.Length; j++)
+        {
+            result[j] = Convert.ToInt32(s[0]) + j;
+        }        
+        return result;
     }
     else
     {
-        int [] array = new int[1];
-        array[0] = Convert.ToInt32(a);
+        int[] result = new int[1];
+        result[0] = Convert.ToInt32(a);
+        return result;
     }
-    return array;
 }
 
-int [] calculationTime (int [] array, int [] a)
+int[] calculationTime(int[] array, int[] a)
 {
     int i = 0;
-    while (i<a.Length)
+    while (i < a.Length)
     {
         array[a[i]]++;
         i++;
@@ -53,25 +54,65 @@ int [] calculationTime (int [] array, int [] a)
     return array;
 }
 
-void Output(int [] array)
+void Output(int[] array)
 {
     int max = 0;
-    for(int i=0; i<array.Length; i++)
+    for (int j = 0; j < array.Length; j++)
     {
-        if(array[i]>max)
+        if (array[j] > max)
         {
-            max=array[i];
+            max = array[j];
         }
     }
-    for(i=0; i<array.Length; i++)
+    bool q = true;
+    int a = 0;
+    int b = 0;
+    string result = string.Empty;
+    int i = 0;
+    while (i < array.Length)
     {
-        if(array[i]==max)
+        if (array[i] == max)
         {
-            while()
+            a = i;
+            q=true;
+            while (q)
+            {
+                if (array[i + 1] == max)
+                {
+                    i++;
+                    b = i;
+                }
+                else
+                {
+                    b = i;
+                    i++;
+                    q = false;
+                }
+            }
+            if (b > a)
+            {
+                result = result + $" {a}-{b},";
+            }
+            else
+            {
+                result = result + $" {a},";
+            }
+        }
+        else
+        {
+            i++;
         }
     }
-    }
+    Console.WriteLine(result);
 }
+
+int n = InputVisitors();
+int[] time = new int[24];
+for (int i = 0; i < n; i++)
+{
+    time = calculationTime(time, Transformation(InputTime()));
+}
+Output(time);
 
 
 
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿ string a = "4-6";
+Console.WriteLine(a.Length);
+
+//  string[] s = a.Split('-');
+//  int i = Convert.ToInt32(s[1]) - Convert.ToInt32(s[0]) + 1;
+//  Console.WriteLine(i); 
+
+//  if (a.Length > 2)
+//     {
+//         string[] s = a.Split('-');
+//         int i = Convert.ToInt32(s[1]) - Convert.ToInt32(s[0]) + 1;
+//         int[] result = new int[i];
+//         for (int j = 0; j < result.Length; j++)
+//         {
+//             result[j] = Convert.ToInt32(s[0]) + j;
+//         }
+//        Console.WriteLine(string.Join(' ', result));
+// //     }
+//     else
+//     {
+//         int[] array = new int[1];
+//         array[0] = Convert.ToInt32(a);
+        
+//     }
+//     Console.WriteLine(string.Join(' ', result));
