36f6c34e366857d4c84d09489afe1002b508654e
Ganibalx <Ganibal1993@gmail.com>
2022-10-11 17:04:08 +0500

    Сделал задачи 12-14

---
+++ Zadachi/Zadacha14/Program.cs
@@ -0,0 +1,35 @@
+﻿int Input()
+{
+    Console.WriteLine("Введите число");
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+void Output(int N)
+{
+    int a = N;
+    int i = 0;
+    while (a>0)
+    {
+        a = a/10;
+        i++;
+    }
+    int [] array = new int[i];
+    i = 0;
+    while (N>0)
+    {
+        array[i] = N % 10;
+        N = N/10;
+        i++;
+    }
+
+    if (array.Length >=3)
+    {
+        Console.WriteLine($"Третье число {array[array.Length-3]}");
+    }
+    else
+    {
+        Console.WriteLine("Третье число отсутствует");
+    }
+}
+
+Output(Input());
