a6f2ebbca4e4113f155ed4edbe51dffa7dbe7886
hitman111 <ganibal1993@gmail.com>
2022-10-17 23:18:06 +0500

    начал делать тетрис

--- Seminari/6/Tetris/Program.cs
+++ Seminari/6/Tetris/Program.cs
@@ -1,49 +1,56 @@
-
-void Figure(int x, int y)
-{  
-  for (int i = x - 1; i <= x + 1; i++)
+char[,] FieldRendering(char[,] array) //Заполнение массива пробелами
+{
+  for(int i = 0; i<15; i++)
   {
-    for (int j = y - 1; j <= y + 1; j++)
+    for(int j = 0; j<20; j++)
     {
-      Console.SetCursorPosition(i, j);
-      Console.Write("#");
+      array[i,j]= ' ';
     }
-  }
+  }    
+  return array;
 }
-int x = 10;
-int y = 2;
+char[,] Frame(char[,] array) // заполнение рамки в массиве
+{
+  for(int i = 0; i<15; i++)
+  {
+    if(i<14)
+    {
+      for(int j = 0; j<20; j+=19)
+    {
+      array[i,j]= '#';
+    }
+    }
+    else
+    {
+      for(int j = 0; j<20; j++)
+    {
+      array[i,j]= '#';
+    }
+    }    
+  }    
+  return array;
+}
+void Output(char[,] array) // Вывод на экран
+{
+  for(int i = 0; i<15; i++)
+  {    
+    for(int j = 0; j<20; j++)
+    {
+      Console.Write(array[i,j]);
+    }
+    Console.WriteLine();
+  }      
+}
 
-// Логика отрисовки всего
+char[,] field = new char[15,20];
+Frame(FieldRendering(field));
 new Thread(() =>
 {
   while (true)
   {
     Console.Clear();
-    Figure(x, y);
-    Thread.Sleep(500);
-    y++;
-    if (y > 15) y = 1;
-  }+
+    Output(field);
+    Console.CursorVisible = false;
+    Thread.Sleep(500);    
+  }
 }).Start();
-
-// Логика проверки нажатия кнопок
-while (true)
-{
-  var key = Console.ReadKey(true).Key;
-
-  if (key == ConsoleKey.LeftArrow)
-  {
-    x--;
-    Figure(x, y);
-  }
-  if (key == ConsoleKey.RightArrow)
-  {
-    x++;
-    Figure(x, y);
-  }
-  // if (key == ConsoleKey.upArrow)
-  // {
-  //   ;
-  // }
-
-}
