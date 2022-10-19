78a06d64a420d52321766cc7adf9bf780a8c29b8
Ganibalx <Ganibal1993@gmail.com>
2022-10-19 17:06:24 +0500

    Занимался тетрисом

---
+++ Zadachi/Zadacha60/Program.cs
@@ -0,0 +1,24 @@
+﻿char [] Count(string a)
+{
+    char[] result = new char[a.Length];
+    bool b = false;
+    for(int i=0; i<a.Length; i++)
+    {
+        b = false;
+        int j = 0;
+        while(!b)
+        {
+            if(a[i] == result[j])
+            {
+                b = true;
+            } 
+            j++;
+        } 
+        if(b)
+        {
+            result
+        }
+    }
+
+
+}
