a998d1b084f8836905f6d99ce7ac7d1af9130c9a
hitman111 <ganibal1993@gmail.com>
2022-11-14 21:13:36 +0500

    сделал 9 дз

--- Seminari/9/HomeWork/Zadacha60/Program.cs
+++ Seminari/9/HomeWork/Zadacha60/Program.cs
@@ -13,16 +13,17 @@
 
 void Filling3D (int [] array, int x, int y, int z)
 {
-    int [, , ,] result = new int[array.Length];
     int flag = 0;
-    for(int i = 0; i<=x ; i++)
-        for(int j = 0; j<=y ; j++)
-            for(int k = 0; k<=z ; k++)
+    for(int i = 0; i<x ; i++)
+        for(int j = 0; j<y ; j++)
+        {
+            for(int k = 0; k<z ; k++)
             {
-                result [i,j,k] =array[flag];
+                Console.Write($"{array[flag]}({i},{j},{k}), ");
                 flag += 1;
             }
-    return result;
+            Console.WriteLine();
+        }     
 }
 
 int x = 4;
@@ -30,4 +31,4 @@ int y = 4;
 int z = 3;
 int [] result = new int [x*y*z];
 //result = Filling(result);
-Console.WriteLine(string.Join(' ', Filling(result)));
+Filling3D(Filling(result), x, y,  z);
